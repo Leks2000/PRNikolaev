8150ce9c01ab2ada9da26af37586249a5626c833
Leks2000 <107043945+Leks2000@users.noreply.github.com>
2023-01-19 14:27:28 +0300

    Add files via upload

---
+++ PR15_1_Nikolaev/PR15_1_Nikolaev/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.Diagnostics;
+
+
+namespace PR15_1_Nikolaev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PdfDocument document= new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            gfx.DrawString("Мой первый PDF - документ", font, XBrushes.Black,
+                new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            string filename = "FirstPDFDocument.pdf";
+            document.Save(filename);
+            Process.Start(filename);
+        }
+    }
+}
